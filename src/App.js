e7bae49588f079ada9c22a6e2a6468b02c5f3a63
nicatzamanwork <webdevelopernicat@gmail.com>
2023-02-22 20:54:17 +0400

    footer is ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar/navbar";
+import Footer from "./components/footer/footer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
