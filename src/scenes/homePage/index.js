764318c23986dc99221d42f38cba24a1747839f8
nicatzamanwork <webdevelopernicat@gmail.com>
2023-03-10 18:30:34 +0400

    flexbetween established

--- src/scenes/homePage/index.js
+++ src/scenes/homePage/index.js
@@ -1,5 +1,11 @@
+import { Box } from "@mui/material";
+import Navbar from "../navbar";
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 };
 
 export default HomePage;
